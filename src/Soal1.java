6eccdee9cdca989e3348723fc6940c62333639de
NeoNetz <aksafadillah@gmail.com>
2024-02-05 17:46:36 +0700

    MAF

---
+++ src/Soal1.java
@@ -0,0 +1,11 @@
+public class Soal1 {
+        public static void main(String[] args) {
+            byte angka1 = 125; //Deklrasi dan inisialisasi angka1 dengan nilai 125
+            byte angka2 = 6; // Deklrasi dan inisialisasi angka2 dengan nilai 6
+            byte hasil = (byte) (angka1+angka2); //Deklarasi variabel byte hasil kemudian menjumlahkan angka 1 dan angka 2
+
+            System.out.println("Hasil 1 = " + hasil);//Menampilkan variabel byte hasil
+
+        }
+    }
+
---
+++ src/Soal2.java
@@ -0,0 +1,16 @@
+public class Soal2 {
+    public static void main(String[] args) {
+        int i = 42;//Deklrasi dan inisiasi variabel int i dengan nilai 42
+        String s = (i<40)?"life":(i>50)?"universe":"everything";//Jika i kurang dari 40 maka nilai s akan menjadi life jika i lebih besar dari 50 maka nilai s akan menjadi universe selain itu nilai s menjadi everything
+        System.out.println(s);//Menampilkan nilai s ke layar
+
+        int z = 30;//Deklrasi dan inisiasi variabel int z dengan nilai 30
+        String p = (z<40)?"life":(z>50)?"universe":"everything";//Jika i kurang dari 40 maka nilai s akan menjadi life jika i lebih besar dari 50 maka nilai s akan menjadi universe selain itu nilai s menjadi everything
+        System.out.println(p);//Menampilkan nilai p ke layar
+        System.out.println("Is");//Menampilkan nilai kalimat "Is" ke layar
+
+        int a = 52;//Deklrasi dan inisiasi variabel int a dengan nilai 52
+        String x = (a<40)?"life":(a>50)?"universe":"everything";//Jika i kurang dari 40 maka nilai s akan menjadi life jika i lebih besar dari 50 maka nilai s akan menjadi universe selain itu nilai s menjadi everything
+        System.out.println(x);//Menampilkan nilai x ke layar
+    }
+}
